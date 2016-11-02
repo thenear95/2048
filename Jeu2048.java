15bcc60892209ccd035496ef339bfe4ff822dfd1
thenear95 <nicolas.sabak@gmail.com>
2016-11-02 15:48:40 +0100

    new version à vérifier (psq paneaux danger)
    a finir les deplacement

--- Jeu2048.java
+++ Jeu2048.java
@@ -37,11 +37,9 @@ public class Jeu2048 extends JFrame implements KeyListener
         
         addKeyListener(this);
         
-        
-        
     }
     
-    public void paint(Graphics g)
+    public void paint(Graphics g) // fonction dessin de la tuile //
     {
         dessinDestuiles(g);
     }
@@ -78,16 +76,11 @@ public class Jeu2048 extends JFrame implements KeyListener
                 g2.drawLine(250, 50, 250, 450);
                 g2.drawLine(350, 50, 350, 450);
                 g2.drawLine(450, 50, 450, 450);
-               
+ // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
                switch(tuiles[i][j])
                {
                    case 2:
-//                       if(tuiles[ii][jj]==2)
-//                       {
-//                       g2.setFont(new Font("Arial",Font.BOLD,40));
-//                       g2.setColor(Color.BLUE.red);
-//                       g2.drawString("2", 90+j*100, 120+i*100);
-//                       }
+
                        g2.setFont(new Font("Arial",Font.BOLD,40));
                        g2.setColor(Color.BLUE.darker());
                        g2.drawString("2", 90+j*100, 120+i*100);
@@ -98,13 +91,9 @@ public class Jeu2048 extends JFrame implements KeyListener
                        g2.setColor(Color.BLUE.darker());
                        g2.drawString("2", 90+j*100, 120+i*100);
                        break;
+                       
                    case 4:
-//                       if(tuiles[ii][jj]==4)
-//                       {
-//                       g2.setFont(new Font("Arial",Font.BOLD,40));
-//                       g2.setColor(Color.red);
-//                       g2.drawString("4", 90+j*100, 120+i*100);
-//                       }
+
                        g2.setFont(new Font("Arial",Font.BOLD,40));
                        g2.setColor(Color.BLUE.darker());
                        g2.drawString("4", 90+j*100, 120+i*100);
@@ -212,6 +201,352 @@ public class Jeu2048 extends JFrame implements KeyListener
             }
         }
         
+        g.drawImage(img, 0, 0, this);
+    }
+    
+    
+    public static void main(String[] args) 
+    {
+      Jeu2048 jeu= new Jeu2048();
+      jeu.setVisible(true);
+      jeu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public void fusiongauche(int x, int y)
+    {
+        int valeur=0;
+        if(tuiles[x][y]%2!=0)
+        {
+            System.out.println("apuuie");
+           
+            valeur= tuiles[x][y];
+            valeur--;
+            tuiles[x][y]=valeur;
+             System.out.println(valeur);
+        }
+        else
+        {
+            
+        valeur= tuiles[x][y];
+        }
+        int ile=0;
+       
+       
+       for(int i=1; i<4;i++)
+           {
+               if(y-i>=0)
+               {
+                   if(tuiles[x][y-i]==0)
+                    {
+                        
+                        tuiles[x][y-i]=valeur;
+                       tuiles[x][y-i+1]=0;
+                       
+                         continue; // internet =D //
+               
+                    }
+                   else if(valeur==tuiles[x][y-i]&&(ile==0))
+                    {
+                        valeur=valeur*2+1;
+                        
+                        tuiles[x][y-i]=valeur;
+                        tuiles[x][y-i+1]=0;
+                        ile++;
+                        
+                        
+                         
+                    }
+                   else
+                     {
+                         
+                         break;
+                     }
+                   
+               }
+           }
         
+    }
+    
+    public void fusionDroite(int x, int y)
+    {
+        int valeur=0;
+        if(tuiles[x][y]%2!=0)
+        {
+            System.out.println("OKE");
+           
+            valeur= tuiles[x][y];
+            valeur--;
+            tuiles[x][y]=valeur;
+             System.out.println(valeur);
+        }
+        else
+        {
+            
+        valeur= tuiles[x][y];
+        }
+        int ile=0;
+       
+       
+       for(int i=1; i<4;i++)
+           {
+               if(y+i<=3)
+               {
+                   if(tuiles[x][y+i]==0)
+                    {
+                        
+                        tuiles[x][y+i]=valeur;
+                       tuiles[x][y+i-1]=0;
+                       
+                         continue;
+               
+                    }
+                   else if(valeur==tuiles[x][y+i]&&(ile==0))
+                    {
+                        valeur=valeur*2+1;
+                        
+                        tuiles[x][y+i]=valeur;
+                        tuiles[x][y+i-1]=0;
+                        ile++;
+                        
+                        
+                         
+                    }
+                   else
+                     {
+                         
+                         break;
+                     }
+                   
+               }
+           }
         
+    }
+    
+     public void ajouterADroite(int x, int y)
+    {
+        int valeur=0;
+        if(tuiles[x][y]%2!=0)
+        {
+            System.out.println("OKE");
+           
+            valeur= tuiles[x][y];
+            valeur--;
+            tuiles[x][y]=valeur;
+             System.out.println(valeur);
+        }
+        else
+        {
+            
+        valeur= tuiles[x][y];
+        }
+        int ile=0;
+       
+       
+       for(int i=1; i<4;i++)
+           {
+               if(x-i>=0)
+               {
+                   if(tuiles[x-i][y]==0)
+                    {
+                        
+                        tuiles[x-i][y]=valeur;
+                       tuiles[x-i+1][y]=0;
+                       
+                         continue;
+               
+                    }
+                   else if(valeur==tuiles[x-i][y]&&(ile==0))
+                    {
+                        valeur=valeur*2+1;
+                        
+                        tuiles[x-i][y]=valeur;
+                        tuiles[x-i+1][y]=0;
+                        ile++;
+
+                    }
+                   else
+                     {
+                         
+                         break;
+                     }
+                   
+               }
+           }
+        
+    }
+     
+
+     public void fusionBas(int x, int y)
+    {
+        int valeur=0;
+        if(tuiles[x][y]%2!=0)
+        {
+            System.out.println("OKE");
+           
+            valeur= tuiles[x][y];
+            valeur--;
+            tuiles[x][y]=valeur;
+             System.out.println(valeur);
+        }
+        else
+        {
+            
+        valeur= tuiles[x][y];
+        }
+        int ile=0;
+       
+       
+       for(int i=1; i<4;i++)
+           {
+               if(x+i<=3)
+               {
+                   if(tuiles[x+i][y]==0)
+                    {
+                        
+                        tuiles[x+i][y]=valeur;
+                       tuiles[x+i-1][y]=0;
+                       
+                         continue;
+               
+                    }
+                   else if(valeur==tuiles[x+i][y]&&(ile==0))
+                    {
+                        valeur=valeur*2+1;
+                        
+                        tuiles[x+i][y]=valeur;
+                        tuiles[x+i-1][y]=0;
+                        ile++;
+                        
+                        
+                         
+                    }
+                   else
+                     {
+                         
+                         break;
+                     }
+                   
+               }
+           }
+        
+    }
+     
+     
+    public void ajouterCaseAlea()
+    {
+        int ile=0;
+        int[]nombreMax={2,4}; // nombres qui sont rajouter au max //
+        ArrayList<Integer> poleI= new ArrayList<Integer>(); 		// instentiation des p
+        ArrayList<Integer> poleJ= new ArrayList<Integer>();
+        Random r= new Random();
+        for(int i=0; i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                if(tuiles[i][j]==0)
+                {
+                    poleI.add(i);
+                    poleJ.add(j);
+                  ile++;
+                  
+                }
+            }
+        }
+        if(ile==0)
+            JOptionPane.showMessageDialog(this, "Fin du jeu");
+        else
+        {
+            int los= r.nextInt(ile);
+             ii=poleI.get(los);
+             jj=poleJ.get(los);
+            int los2=r.nextInt(2);
+            if(los2==0)
+            {
+               tuiles[ii][jj]=2; 
+            }
+            else
+            {
+                 tuiles[ii][jj]=4;
+                
+            }
+        }
+    }
+  
+    
    
+
+    
+// pour les touches du clavier ICI // 
+    // Faire comme avec les boutons en cours //
+    // Gestionnaires de mise en forme !!!!!!!! //
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        int k=e.getKeyCode();
+        
+        switch(k)								
+                {
+            case 39:										// DROITE //
+                
+                for(int i=0;i<4;i++)
+                {
+                  for(int j=3;j>=0;j--)
+                  {
+                      fusionDroite(i, j);
+                  } 
+                }
+                ajouterCaseAlea();
+                System.out.println("Touche droite");
+                break;
+                
+                
+            case 40:								// EN BAS //
+                for(int i=3;i>=0;i--)
+                {
+                  for(int j=0;j<4;j++)
+                  {
+                      fusionBas(i, j);
+                  } 
+                }
+                ajouterCaseAlea();
+                System.out.println("Touche bas");
+                break;
+                
+                
+            case 38:			
+                for(int i=0;i<4;i++)
+                {
+                  for(int j=0;j<4;j++)
+                  {
+                      ajouterADroite(i, j);
+                  } 
+                }
+                ajouterCaseAlea();
+                System.out.println("Touche haut");
+                break;
+            case 37:
+                for(int i=0;i<4;i++)
+                {
+                  for(int j=0;j<4;j++)
+                  {
+                      fusiongauche(i, j);
+                  } 
+                }
+               ajouterCaseAlea();
+                System.out.println("Touche gauche");
+                break;    
+                    
+                }
+        
+        repaint(); // MAJ du panel au niveau des graphismes  apl seulement paint //
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+    
+}
